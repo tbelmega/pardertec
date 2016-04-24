190f119cd170b5f05f737fcbbfc86c22be1954be
t.belmega@gmx.de <7B9deeyyUpjmqwNnX1Ej>
2016-04-24 15:47:04 +0100

    Implement recipe detail view

--- SmartMeal/app/src/main/java/de/pardertec/smartmeal/recipes/detail/RecipeDetailActivity.java
+++ SmartMeal/app/src/main/java/de/pardertec/smartmeal/recipes/detail/RecipeDetailActivity.java
@@ -2,17 +2,25 @@ package de.pardertec.smartmeal.recipes.detail;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import de.pardertec.datamodel.Ingredient;
 import de.pardertec.datamodel.Recipe;
 import de.pardertec.datamodel.RecipeCollection;
+import de.pardertec.datamodel.RecipeStep;
 import de.pardertec.smartmeal.R;
 import de.pardertec.smartmeal.SmartMealApplication;
 
@@ -21,11 +29,16 @@ import static de.pardertec.smartmeal.SmartMealApplication.EXTRA_RECIPE_ID;
 public class RecipeDetailActivity extends AppCompatActivity {
 
     Recipe currentRecipe;
+    private RecipeDetailActivityFragment ingredientsDetailFragment;
+
     private TextView titleTextView;
     private TextView textTextView;
     private TextView durationTextView;
     private TextView difficultyTextView;
     private TextView servingsTextView;
+
+    private ListView ingredientsList;
+    private ListView stepList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +47,17 @@ public class RecipeDetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ingredientsDetailFragment = (RecipeDetailActivityFragment) getFragmentManager().
+                findFragmentById(R.id.recipe_detail_fragment);
+
         titleTextView = (TextView) findViewById(R.id.recipe_title);
         textTextView = (TextView) findViewById(R.id.recipe_text);
         durationTextView = (TextView) findViewById(R.id.duration);
         difficultyTextView = (TextView) findViewById(R.id.difficulty);
         servingsTextView = (TextView) findViewById(R.id.servings);
+
+        ingredientsList = (ListView) findViewById(R.id.ingredients_list);
+        stepList= (ListView) findViewById(R.id.steps_list);
     }
 
 
@@ -47,26 +66,45 @@ public class RecipeDetailActivity extends AppCompatActivity {
         super.onResume();
 
         Intent intent = getIntent();
-        currentRecipe = load(intent.getStringExtra(EXTRA_RECIPE_ID));
+        currentRecipe = loadRecipe(intent.getStringExtra(EXTRA_RECIPE_ID));
 
         titleTextView.setText(currentRecipe.getName());
         textTextView.setText(currentRecipe.getText());
         durationTextView.setText(currentRecipe.getDuration() + " " +
                 getResources().getString(R.string.minutes));
-        servingsTextView.setText(
-                getResources().getString(R.string.servings_for_persons).replace(
-                        getResources().getString(R.string.placeholder),
-                        currentRecipe.getServings().toString()));
+        servingsTextView.setText(createServingsText(currentRecipe));
         difficultyTextView.setText(currentRecipe.getDifficulty().toString());
 
-        //show ingrdients
+        //ingredients list
+        List<IngredientEntry> ingredients = getIngredientEntryList(currentRecipe);
+        ArrayAdapter<IngredientEntry> ingredientEntryArrayAdapter = new ArrayAdapter<>(this,
+                R.layout.simple_list_item, ingredients);
+        ingredientsList.setAdapter(ingredientEntryArrayAdapter);
+        ViewGroup layout = (ViewGroup) findViewById(R.id.scrollview_content);
+        layout.requestLayout();
 
-        //show steps
+        /*
+         * Horrible hack: in order to calculate the needed height for the list views,
+         * I take naively 80% of the screen size as width
+         */
+        int width = (int) (getWindowManager().getDefaultDisplay().getWidth() * 0.8);
+        fixListViewHeightBasedOnChildren(ingredientsList, width);
+
+        //steps list
+        List<RecipeStep> steps = currentRecipe.getStepsCopy();
+        ArrayAdapter<RecipeStep> stepArrayAdapter = new ArrayAdapter<>(this,
+                R.layout.simple_list_item, steps);
+        stepList.setAdapter(stepArrayAdapter);
+        fixListViewHeightBasedOnChildren(stepList, width);
+        RelativeLayout.LayoutParams stepListParams = (RelativeLayout.LayoutParams) stepList.getLayoutParams();
+        stepListParams.addRule(RelativeLayout.BELOW, R.id.steps_header);
+        stepList.setLayoutParams(stepListParams);
 
         //show image
     }
 
-    private Recipe load(String id) {
+
+    private Recipe loadRecipe(String id) {
         RecipeCollection collection = ((SmartMealApplication)getApplication()).getRecipeCollection();
         return collection.getRecipe(id);
     }
@@ -91,5 +129,64 @@ public class RecipeDetailActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Replace the PLACEHOLDER within the textSkeleton by the actual number of servings.
+     */
+    private String createServingsText(Recipe currentRecipe) {
+        String textSkeleton = getResources().getString(R.string.servings_for_persons);
+        String placeholder = getResources().getString(R.string.placeholder);
+        String numberOfServings = currentRecipe.getServings().toString();
+        return textSkeleton.replace(placeholder, numberOfServings);
+    }
+
+
+    private List<IngredientEntry> getIngredientEntryList(Recipe currentRecipe) {
+        List<IngredientEntry> ingredients = new LinkedList<>();
+
+        for (Ingredient ingredient: currentRecipe.getIngredients().keySet()) {
+            Integer amount = currentRecipe.getIngredients().get(ingredient);
+            ingredients.add(new IngredientEntry(ingredient, amount));
+        }
+        return ingredients;
+    }
+
+
+    private static void fixListViewHeightBasedOnChildren(ListView listView, int width) {
+        listView.setEnabled(false);
+        ViewGroup.LayoutParams par = listView.getLayoutParams();
+        par.height = calcListViewHeightBasedOnChildren(listView,  width) + (listView.getDividerHeight() * (listView.getAdapter().getCount() - 1));
+        listView.setLayoutParams(par);
+        listView.requestLayout();
+    }
+
+    private static int calcListViewHeightBasedOnChildren(ListView listView, int width) {
+        ListAdapter adapter = listView.getAdapter();
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.AT_MOST);
+
+        int totalHeight = listView.getPaddingTop() + listView.getPaddingBottom();
+        for (int i = 0; i < adapter.getCount(); i++) {
+            View listItem = adapter.getView(i, null, listView);
+            listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+        return totalHeight;
+    }
+
+
+    private class IngredientEntry {
+        private final Ingredient ingredient;
+        private final Integer amount;
+
+        public IngredientEntry(Ingredient ingredient, Integer amount) {
+            this.ingredient = ingredient;
+            this.amount = amount;
+        }
+
+        @Override
+        public String toString() {
+            return  ingredient.getName()+ ": " + amount + " " + ingredient.getMeasure();
+        }
     }
 }
--- SmartMeal/app/src/main/java/de/pardertec/smartmeal/recipes/detail/RecipeDetailActivityFragment.java
+++ SmartMeal/app/src/main/java/de/pardertec/smartmeal/recipes/detail/RecipeDetailActivityFragment.java
@@ -1,6 +1,6 @@
 package de.pardertec.smartmeal.recipes.detail;
 
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,12 +13,10 @@ import de.pardertec.smartmeal.R;
  */
 public class RecipeDetailActivityFragment extends Fragment {
 
-    public RecipeDetailActivityFragment() {
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_recipe_detail, container, false);
     }
+
 }
